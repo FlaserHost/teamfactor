59d23ef38a1c7e64747e77b895bc5502fb0b6704
Dmitry <flaserhost@gmail.com>
2023-12-24 23:38:32 +0300

    header

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const navBtns = document.querySelectorAll('.nav-btn');
     navBtns.forEach((btn, _, orig) => {
         btn.addEventListener('click', e => {
-            subSections.forEach(sub => sub.classList.remove('showed'));
+
 
             const subId = e.target.dataset.subId;
             const subSection = document.getElementById(subId);
 
             if (!e.target.classList.contains('active-nav')) {
+                subSections.forEach(sub => sub.classList.remove('showed'));
+                orig.forEach(nav => nav.classList.remove('active-nav'));
                 e.target.classList.add('active-nav');
                 subSection.classList.add('showed');
             } else {
-                orig.forEach(nav => nav.classList.remove('active-nav'));
+                e.target.classList.remove('active-nav');
+                subSection.classList.remove('showed');
             }
         });
     });
