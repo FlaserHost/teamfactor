046b798b35c5566fded36fc342855cd80ae7419b
Dmitry <flaserhost@gmail.com>
2024-04-02 10:40:02 +0300

    test

--- src/js/script.js
+++ src/js/script.js
@@ -204,7 +204,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const pageLang = document.querySelector('.page-wrapper').className.split(' ')[1];
 
         const scrollCorrections = {
-            english: 612,
+            english: 1612,
             turkish: 342
         };
 
