11720f7d6a2aadafafee27f16199f57637397bc1
Dmitry <flaserhost@gmail.com>
2024-05-31 10:25:59 +0300

    maxWidth

--- src/js/modal_form.js
+++ src/js/modal_form.js
@@ -1,6 +1,4 @@
 // открытие демо формы
-const html = document.querySelector('html');
-
 const demoModal = document.querySelector('.demo-page-modal');
 const demoModalTitle = demoModal.querySelector('h2');
 const demoModalDescription = demoModal.querySelector('p');
