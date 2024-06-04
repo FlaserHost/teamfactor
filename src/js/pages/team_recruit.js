3ad9081ebe3b792b58afb6733b7ae303b1eaeb90
Dmitry <flaserhost@gmail.com>
2024-06-04 16:39:42 +0300

    corr

--- src/js/pages/team_recruit.js
+++ src/js/pages/team_recruit.js
@@ -3,9 +3,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sliderConditions = new Swiper('.swiper-conditions', {
         direction: 'horizontal',
-        slidesPerView: 'auto',
         loop: true,
         spaceBetween: 17,
+        breakpoints: {
+            10: {
+                slidesPerView: 1,
+                loop: true,
+            },
+            500: {
+                slidesPerView: 2,
+                loop: true,
+            },
+            700: {
+                slidesPerView: 'auto',
+                loop: true,
+            }
+        },
         navigation: {
             nextEl: '.conditions-slider-nav-btn.next',
             prevEl: '.conditions-slider-nav-btn.prev',
